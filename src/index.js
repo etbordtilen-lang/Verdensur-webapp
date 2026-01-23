433820b848340902deafc5a640210ba8971421d1
etbordtilen-lang <etbordtilen@gmail.com>
2026-01-23 17:35:00 +0100

    Added moment js cdn, moment timezone cdn and added updateTime function in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+function updateTime() {
+  let rowOneElement = document.querySelector("#row-one");
+  let rowOneDateElement = rowOneElement.querySelector(".date");
+  let rowOneTimeElement = rowOneElement.querySelector(".time");
+  let rowOneTime = moment().tz("Australia/Sydney");
 
+  rowOneDateElement.innerHTML = rowOneTime.format("MMMM Do YYYY");
+  rowOneTimeElement.innerHTML = `${rowOneTime.format("h:mm:ss")} <small>${rowOneTime.format("A")}</small>`;
+
+  // Repeat for row two
+  let rowTwoElement = document.querySelector("#row-two");
+  let rowTwoDateElement = rowTwoElement.querySelector(".date");
+  let rowTwoTimeElement = rowTwoElement.querySelector(".time");
+  let rowTwoTime = moment().tz("Asia/Hong_Kong");
+
+  rowTwoDateElement.innerHTML = rowTwoTime.format("MMMM Do YYYY");
+  rowTwoTimeElement.innerHTML = `${rowTwoTime.format("h:mm:ss")} <small>${rowTwoTime.format("A")}</small>`;
+
+  // Repeat for row three
+  let rowThreeElement = document.querySelector("#row-three");
+  let rowThreeDateElement = rowThreeElement.querySelector(".date");
+  let rowThreeTimeElement = rowThreeElement.querySelector(".time");
+  let rowThreeTime = moment().tz("Europe/London");
+
+  rowThreeDateElement.innerHTML = rowThreeTime.format("MMMM Do YYYY");
+  rowThreeTimeElement.innerHTML = `${rowThreeTime.format("h:mm:ss")} <small>${rowThreeTime.format("A")}</small>`;
+
+  // Repeat for row four
+  let rowFourElement = document.querySelector("#row-four");
+  let rowFourDateElement = rowFourElement.querySelector(".date");
+  let rowFourTimeElement = rowFourElement.querySelector(".time");
+  let rowFourTime = moment().tz("America/New_York");
+
+  rowFourDateElement.innerHTML = rowFourTime.format("MMMM Do YYYY");
+  rowFourTimeElement.innerHTML = `${rowFourTime.format("h:mm:ss")} <small>${rowFourTime.format("A")}</small>`;
+}
+updateTime(); // Initial call to set time immediately
+
+setInterval(updateTime, 1000);
