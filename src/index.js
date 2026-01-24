fc0794a598a7f9e286d6cf626d7e79ee2a70c4e1
etbordtilen-lang <etbordtilen@gmail.com>
2026-01-24 14:40:14 +0100

    Updated the js functionalities
    So that the current city time stays static and the selected city is added below so the user can compare times

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
-function updateTime() {
-  let rowOneElement = document.querySelector("#row-one");
-  let rowOneDateElement = rowOneElement.querySelector(".date");
-  let rowOneTimeElement = rowOneElement.querySelector(".time");
-  let rowOneTime = moment().tz("UTC");
+let selectedCityTimeZone = null;
 
-  rowOneDateElement.innerHTML = rowOneTime.format("MMMM Do YYYY");
-  rowOneTimeElement.innerHTML = `${rowOneTime.format("h:mm:ss")} <small>${rowOneTime.format("A")}</small>`;
+function updateTime() {
+  let rowOneElement = document.querySelector("#current-location");
+  if (rowOneElement) {
+    let rowOneDateElement = rowOneElement.querySelector(".current-date");
+    let rowOneTimeElement = rowOneElement.querySelector(".current-time");
+    let rowOneHeader = rowOneElement.querySelector(".current-city");
+    let userTimeZone = moment.tz.guess();
+    let userCityName = userTimeZone.replace("_", " ").split("/")[1];
+    let userTime = moment().tz(userTimeZone);
+
+    rowOneHeader.innerHTML = `${userCityName}`;
+    rowOneDateElement.innerHTML = userTime.format("MMMM Do YYYY");
+    rowOneTimeElement.innerHTML = `${userTime.format("h:mm:ss")} <small>${userTime.format("A")}</small>`;
+  }
+
+  if (selectedCityTimeZone) {
+    let cityName = selectedCityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(selectedCityTimeZone);
+    let selectedCityElement = document.querySelector("#selected-city");
+    selectedCityElement.innerHTML = `
+    <div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+    </div>`;
+  }
 }
 
 function updateCityTime(event) {
-  let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  //console.log(cityTime.format("MMM Do YYYY"));
-  let citiesElement = document.querySelector(".cities");
-  citiesElement.innerHTML += ` 
-  <div class="city">
-    <div>
-        <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div>
-            <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-          </div>
-          </div>`;
+  selectedCityTimeZone = event.target.value;
+
+  updateTime();
 }
 
-updateTime(); // Initial call to set time immediately
+updateTime();
 setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#timezone-select");
